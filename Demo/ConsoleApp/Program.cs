5f34e04e4075d9edc21e32ece13ac3d84df9a6ad
willhuo <willhuo@outlook.com>
2019-10-10 19:12:10 +0800

    add demo project

---
+++ Demo/ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        [DllImport("dlls\\NewLib")]
+        static extern int NativeSum(int arg1, int arg2);
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                int thirty = NativeSum(10, 20);
+                Console.WriteLine($"NativeSum(10,20) = {thirty}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message} Line: {e.Source}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
